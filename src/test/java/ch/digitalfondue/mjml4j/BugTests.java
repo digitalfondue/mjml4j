177bb1da08cea98d00ac8981bb5a6f4750b0b621
caluwaertp <peter.caluwaert@gmail.com>
2025-04-30 19:22:14 +0200

    fix nfe (=NumberFormatException) when using inner-border on a mj-column (#10)
    Co-authored-by: Peter Caluwaert <peter.caluwaert.ext@bosa.fgov.be>

--- src/test/java/ch/digitalfondue/mjml4j/BugTests.java
+++ src/test/java/ch/digitalfondue/mjml4j/BugTests.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.Test;
 import static ch.digitalfondue.mjml4j.Helpers.testTemplate;
 
 class BugTests {
-    
+
     // see https://github.com/digitalfondue/mjml4j/issues/7
     @Test
     void checkMjSocialNpe() {
         testTemplate("bug-mj-social");
+    }
+
+    @Test
+    void checkMjColumnInnerBorderNfe() {
+        testTemplate("bug-mj-column-inner-border");
     }
 }
