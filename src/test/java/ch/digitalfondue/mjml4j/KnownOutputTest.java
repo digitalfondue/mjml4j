1b538a02fbcea34f2074fbc31a62d7182b8ef227
Sylvain Jermini <sylvain.jermini@syjer.com>
2026-05-14 17:13:17 +0200

    check against html

---
+++ src/test/java/ch/digitalfondue/mjml4j/KnownOutputTest.java
@@ -0,0 +1,22 @@
+package ch.digitalfondue.mjml4j;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+// ensure we generate a stable HTML
+class KnownOutputTest {
+
+  // this test has various tricky issues,
+  // like the "<" and ">" in raw sections, they should not be interpreted as entities
+  // text inside the table in wrong locations: they should not be moved around
+  @Test
+  void checkAgainstRawSectionColumn() throws IOException {
+    var result = Mjml4j.render(Files.readString(Path.of("data/components/raw-section-column.mjml"), StandardCharsets.UTF_8));
+    Assertions.assertEquals(Files.readString(Path.of("data/html-output/raw-section-column.html"), StandardCharsets.UTF_8), result);
+  }
+}
