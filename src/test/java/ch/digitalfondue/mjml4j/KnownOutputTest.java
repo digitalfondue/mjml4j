8b10097ca757157f7861ff804c35a178d2a3fdb4
Sylvain Jermini <sylvain.jermini@syjer.com>
2026-05-15 10:09:57 +0200

    switch to pnpm

--- src/test/java/ch/digitalfondue/mjml4j/KnownOutputTest.java
+++ src/test/java/ch/digitalfondue/mjml4j/KnownOutputTest.java
@@ -1,12 +1,11 @@
 package ch.digitalfondue.mjml4j;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 // ensure we generate a stable HTML
 class KnownOutputTest {
@@ -16,7 +15,13 @@ class KnownOutputTest {
   // text inside the table in wrong locations: they should not be moved around
   @Test
   void checkAgainstRawSectionColumn() throws IOException {
-    var result = Mjml4j.render(Files.readString(Path.of("data/components/raw-section-column.mjml"), StandardCharsets.UTF_8));
-    Assertions.assertEquals(Files.readString(Path.of("data/html-output/raw-section-column.html"), StandardCharsets.UTF_8), result);
+    var result =
+        Mjml4j.render(
+            Files.readString(
+                Path.of("data/components/raw-section-column.mjml"), StandardCharsets.UTF_8));
+    Assertions.assertEquals(
+        Files.readString(
+            Path.of("data/html-output/raw-section-column.html"), StandardCharsets.UTF_8),
+        result);
   }
 }
