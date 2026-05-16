88165af8733491f1871719c5ab862a443e0218d0
Sylvain Jermini <sylvain.jermini@syjer.com>
2026-05-16 15:38:08 +0200

    wip mj-class child handling

--- src/main/java/ch/digitalfondue/mjml4j/MjmlComponentHeadAttributes.java
+++ src/main/java/ch/digitalfondue/mjml4j/MjmlComponentHeadAttributes.java
@@ -40,18 +40,41 @@ class MjmlComponentHeadAttributes extends BaseComponent.HeadComponent {
   private void handleElement(Element element) {
     if ("mj-class".equals(element.getTagName())) {
       var className = element.getAttribute("name");
-      if (className != null) {
-        var attributesLength = element.getAttributes().getLength();
-        var attributes = element.getAttributes();
-        for (var i = 0; i < attributesLength; i++) {
-          var attribute = (Attr) attributes.item(i);
-          var attributeName = attribute.getName();
-          var attributeValue = attribute.getValue();
-          if (!"name".equals(attributeName)) {
-            context.setClassAttribute(attributeName, className, attributeValue);
+      if (Utils.isNullOrWhiteSpace(className)) {
+        return;
+      }
+
+      var attributesLength = element.getAttributes().getLength();
+      var attributes = element.getAttributes();
+      for (var i = 0; i < attributesLength; i++) {
+        var attribute = (Attr) attributes.item(i);
+        var attributeName = attribute.getName();
+        var attributeValue = attribute.getValue();
+        if (!"name".equals(attributeName)) {
+          context.setClassAttribute(attributeName, className, attributeValue);
+        }
+      }
+      // handle child elements
+      if (element.hasChildNodes()) {
+        var childNodes = element.getChildNodes();
+        int childCount = childNodes.getLength();
+        for (var i = 0; i < childCount; i++) {
+          var childNode = childNodes.item(i);
+          if (childNode.getNodeType() == Node.ELEMENT_NODE
+              && childNode instanceof Element childElement) {
+            String childTagName = childElement.getTagName();
+            var childAttributesLength = childElement.getAttributes().getLength();
+            var childAttributes = childElement.getAttributes();
+            for (var j = 0; j < childAttributesLength; j++) {
+              var childAttribute = (Attr) childAttributes.item(j);
+              var attributeName = childAttribute.getName();
+              var attributeValue = childAttribute.getValue();
+              System.err.println(childTagName + " " + attributeName + " " + attributeValue);
+            }
           }
         }
       }
+      //
     } else {
       var tagName = element.getTagName();
       var attributesLength = element.getAttributes().getLength();
